0c291235672a8dc6fe403efd1d2a22d05e8e6199
davidchenbest <davidchen108@gmail.com>
2020-09-10 21:24:23 -0400

    'first'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div >
+    <template v-for="(q,index) in questions" >
+      <div :class='{correct:answer[index],wrong:wrong[index]}' :key='index'>
+         
+        <card  :question="q" :id='index' v-on:answer='updateAnswer($event)' :checklist='result'></card>
+        <!-- <p :class='{display:!wrong[index]}'>Wrong{{wrong[index]}}</p> -->
+      </div>
+    </template>
+    <button  @click="submit">Submit</button>
+    <p>{{error}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    card:Card
+  },
+  methods:{
+    submit:function(){
+      this.answer.forEach((item,i)=>{
+        if(item === null) this.unanswered.push(i);        
+        else if(item == this.questions[i].answer ){
+          this.result[i] =true
+        }
+        else  this.result[i] =false
+      })
+      if(this.unanswered.length > 0) {
+        this.error = 'not finished'
+        this.unanswered=[]
+      }
+      else {
+        this.error=''
+        //console.log(this.result);
+        }
+      
+      this.result=[...this.result]
+    },
+    updateAnswer:function(a){
+      
+      if(this.questions[a.index].answer == a.answer){
+        this.answer[a.index]=true
+      }
+      else this.answer[a.index]=false
+      console.log(this.answer);
+    }
+  },
+  data:function(){
+    return{
+      questions:[
+        {question:'what is 5+5?', a:1, b:2, c:3, d:10, answer:10 },
+        {question:'what is 5+6?', a:1, b:2, c:3, d:11, answer:11 }
+      ],
+      answer:[],
+      unanswered:[],
+      result:[],
+      wrong:[],
+      error:'',
+      
+      
+    }
+  },
+  mounted:function(){
+    for(let i=0; i< this.questions.length; i++){
+      this.answer[i]=null;
+      this.wrong[i]=null;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .correct{
+    border: 3px solid green;
+  }
+  .wrong{
+    background-color: red;
+  }
+  .display{
+    display:none;
+  }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p>{{id+1}}. {{question.question}}</p>
+        <input type="radio" :name='id' :value="question.a" @click="choose"><label>{{question.a}}</label><br>
+        <input type="radio" :name='id' :value="question.b" @click="choose"><label>{{question.b}}</label><br>
+        <input type="radio" :name='id' :value="question.c" @click="choose"><label>{{question.c}}</label><br>
+        <input type="radio" :name='id' :value="question.d" @click="choose"><label>{{question.d}}</label>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['question','id'],
+    data () {
+        return {
+            answer: null,
+        }
+    },
+    methods:{
+        choose:function(e){
+            this.answer = e.target.value
+            this.$emit('answer', {index:e.toElement.name,answer:this.answer})
+        }
+    },
+    mounted:function(){
+        
+    }
+}
+</script>
+<style scoped>
+    
+    
+    
+</style>
